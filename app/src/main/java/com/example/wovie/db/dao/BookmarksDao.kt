dcfaa27a0b6eb343bfe460b85b73ff14315f9248
Evgeniy Burovnikov <EvgeniyBurovnikov@yandex.ru>
2022-11-26 15:24:09 +0300

    init db

---
+++ app/src/main/java/com/example/wovie/db/dao/BookmarksDao.kt
@@ -0,0 +1,23 @@
+package com.example.wovie.db.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.wovie.db.entity.BookmarkEntity
+
+
+@Dao
+interface BookmarksDao {
+    @Insert
+    suspend fun insert(bookmark: BookmarkEntity)
+
+    @Delete
+    suspend fun delete(bookmark: BookmarkEntity)
+
+    @Query("SELECT * FROM bookmarks")
+    suspend fun getAll(): List<BookmarkEntity>
+
+    @Query("DELETE FROM bookmarks")
+    suspend fun clear()
+}
---
+++ app/src/main/java/com/example/wovie/db/entity/BookmarkEntity.kt
@@ -0,0 +1,12 @@
+package com.example.wovie.db.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "bookmarks")
+data class BookmarkEntity  (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id") val pKey: Int,
+    @ColumnInfo(name = "movie_id") val movieId: Int
+)
