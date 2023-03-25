9ba21c56c6b2b1b84939239412e3de0c75c94202
sonya <sofiya.fedorova@mail.ru>
2023-03-26 01:00:12 +0300

    CheckExistSearchResultTest and SearchScreen methods

---
+++ app/src/androidTest/java/com/example/wovie/ui/CheckExistSearchResultTest.kt
@@ -0,0 +1,39 @@
+package com.example.wovie.ui
+
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.wovie.ui.screens.MainScreen
+import com.example.wovie.ui.screens.SearchScreen
+import com.example.wovie.util.IdlingResource
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class CheckExistSearchResultTest {
+    @get:Rule var activityScenarioRule = activityScenarioRule<MainActivity>()
+
+    @Before
+    fun before() {
+        IdlingRegistry.getInstance().register(IdlingResource.countingIdlingResource)
+    }
+
+    @After
+    fun after() {
+        IdlingRegistry.getInstance().unregister(IdlingResource.countingIdlingResource)
+    }
+
+    @Test
+    fun checkExistSearchResultTest() {
+        val mainScreen = MainScreen()
+        mainScreen
+            .clickOnSearchOnAppBar()
+            .enterSearchRequest("The Lion King")
+            .checkNameOfFirstSearchResult("The Lion King")
+    }
+}
--- app/src/androidTest/java/com/example/wovie/ui/screens/SearchScreen.kt
+++ app/src/androidTest/java/com/example/wovie/ui/screens/SearchScreen.kt
@@ -1,15 +1,43 @@
 package com.example.wovie.ui.screens
 
+import android.view.View
+import android.widget.SearchView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.example.wovie.R
+import com.example.wovie.ui.main.FilmViewHolder
+import com.example.wovie.ui.search.SearchViewHolder
+import com.example.wovie.ui.utils.RecyclerViewMatcher
 
 class SearchScreen {
     fun checkScreenTitle() {
         onView(withId(R.id.top_title))
             .check(ViewAssertions.matches(withText("Search")))
+    }
+
+    fun clickOnFirstFilm(): FilmScreen {
+        onView(withId(R.id.search_results))
+            .perform(
+                RecyclerViewActions
+                .actionOnItemAtPosition<SearchViewHolder>(0, ViewActions.click()))
+        return FilmScreen()
+    }
+
+    fun enterSearchRequest(text: String): SearchScreen {
+        onView(withId(R.id.search_view))
+            .perform(ViewActions.click())
+            .perform(ViewActions.typeText(text))
+        return this
+    }
+
+    fun checkNameOfFirstSearchResult(text: String) {
+        onView(RecyclerViewMatcher(R.id.search_results)
+            .atPositionOnView(0, R.id.search_result_holder))
+            .check(ViewAssertions.matches(withText(text)))
     }
 }
