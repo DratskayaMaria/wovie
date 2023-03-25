b53b738114e7aa250ff983c9597fba9b1329ea31
Maria Dratskaya <dratskaya.m@mail.ru>
2023-03-26 00:32:02 +0300

    ui tests maria: film screen

---
+++ app/src/androidTest/java/com/example/wovie/ui/CheckFilmScreenFromMainScreenTest.kt
@@ -0,0 +1,50 @@
+package com.example.wovie.ui
+
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.ext.junit.rules.activityScenarioRule
+import com.example.wovie.ui.screens.FilmScreen
+import com.example.wovie.ui.screens.MainScreen
+import com.example.wovie.util.IdlingResource
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class CheckFilmScreenFromMainScreenTest {
+    @get:Rule
+    var activityScenarioRule = activityScenarioRule<MainActivity>()
+
+    @Before
+    fun before() {
+        IdlingRegistry.getInstance().register(IdlingResource.countingIdlingResource)
+    }
+
+    @After
+    fun after() {
+        IdlingRegistry.getInstance().unregister(IdlingResource.countingIdlingResource)
+    }
+
+    @Test
+    fun checkFilmScreenFromMainScreenTest() {
+        val mainScreen = MainScreen()
+        mainScreen
+            .clickOnFirstFilm()
+            .checkFilmName()
+            .checkFilmCover()
+            .checkFilmPoster()
+            .checkFilmGenresHeading()
+            .checkFilmGenres()
+            .checkFilmDateTitle()
+            .checkFilmDate()
+            .checkFilmRatingHeading()
+            .checkFilmRating()
+            .checkFilmVotersHeading()
+            .checkFilmVoters()
+            .checkFilmOverviewHeading()
+            .checkFilmOverview()
+            .checkFilmActorsTitle()
+            .checkFilmActorsList()
+            .checkFilmRecommendedTitle()
+            .checkFilmRecommendedList()
+    }
+}
