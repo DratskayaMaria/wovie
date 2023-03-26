7686d4d40b6d6a31149a9d2a0659042f908f387e
sonya <sofiya.fedorova@mail.ru>
2023-03-26 11:58:42 +0300

    CheckNonExistSearchResultTest

--- app/src/androidTest/java/com/example/wovie/ui/screens/SearchScreen.kt
+++ app/src/androidTest/java/com/example/wovie/ui/screens/SearchScreen.kt
@@ -55,20 +55,9 @@ class SearchScreen {
             .check(ViewAssertions.matches(withText(text)))
     }
 
-//    private fun typeSearchViewText(text: String): ViewAction {
-//        return object : ViewAction {
-//            override fun getDescription(): String {
-//                return "Change view text"
-//            }
-//
-//            override fun getConstraints(): Matcher<View> {
-//                return allOf(isDisplayed(), isAssignableFrom(SearchView::class.java))
-//            }
-//
-//            override fun perform(uiController: UiController?, view: View?) {
-//                (view as SearchView).setQuery(text, true)
-//            }
-//        }
-//    }
+    fun checkNoResultsVisible() {
+        onView(withId(R.id.no_results_layout))
+            .check(ViewAssertions.matches(isDisplayed()))
+    }
 
 }
