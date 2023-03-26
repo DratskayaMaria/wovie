54dd4937e3799452cdcd6e55ed5a059091208970
sonya <sofiya.fedorova@mail.ru>
2023-03-26 14:56:54 +0300

    NoInternetConnectionTest

---
+++ app/src/androidTest/java/com/example/wovie/ui/NoInternetConnectionTest.kt
@@ -0,0 +1,42 @@
+package com.example.wovie.ui
+
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import com.example.wovie.ui.screens.MainScreen
+import com.example.wovie.ui.screens.SearchScreen
+import com.example.wovie.util.IdlingResource
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class NoInternetConnectionTest {
+    @get:Rule var activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Before
+    fun before() {
+        IdlingRegistry.getInstance().register(IdlingResource.countingIdlingResource)
+        InstrumentationRegistry.getInstrumentation().uiAutomation.executeShellCommand("svc wifi disable")
+        InstrumentationRegistry.getInstrumentation().uiAutomation.executeShellCommand("svc data disable")
+    }
+
+    @After
+    fun after() {
+        IdlingRegistry.getInstance().unregister(IdlingResource.countingIdlingResource)
+        InstrumentationRegistry.getInstrumentation().uiAutomation.executeShellCommand("svc wifi enable")
+        InstrumentationRegistry.getInstrumentation().uiAutomation.executeShellCommand("svc data enable")
+    }
+
+    @Test
+    fun checkExistSearchResultTest() {
+        val mainScreen = MainScreen(activityRule)
+        mainScreen.isNoInternetMessageDisplayed()
+    }
+}
